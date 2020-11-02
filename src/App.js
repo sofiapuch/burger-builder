9d23b46f5148b65404fc56f8ac4e9a09b9fc06c6
Sofia Puch <sofiapuch.frontend@gmail.com>
2020-11-02 15:22:13 +0100

    Added hoc component, changed App to class-based component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App() {
-  return (
-    <div className="App">Test font</div>
-  );
+import React, { Component } from 'react';
+
+import Layout from './components/Layout/index';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <p>Test</p>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
