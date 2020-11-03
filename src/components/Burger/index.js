41e1169b76d9c81c839397fad6d797a148675d92
Sofia Puch <sofiapuch.frontend@gmail.com>
2020-11-03 11:02:42 +0100

    Burger component created

---
+++ src/components/Burger/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Burger extends Component {
+
+    render() {
+        return (
+            <div>
+                Call Burger Ingredients
+            </div>
+        );
+    }
+}
+
+export default Burger;
